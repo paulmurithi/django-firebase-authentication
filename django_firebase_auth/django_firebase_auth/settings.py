f630265d93f1fc9e6c6a6d7e3e6246eb2ae9be7f
Murithi paul <murithipaul1@gmail.com>
2021-03-04 20:33:41 +0300

    initialize firebase app

--- django_firebase_auth/django_firebase_auth/settings.py
+++ django_firebase_auth/django_firebase_auth/settings.py
@@ -118,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+GOOGLE_APPLICATION_CREDENTIALS = '/home/programmer/Downloads/grodigi-d809f-firebase-adminsdk-bgxm4-61f5bf15b9.json'
